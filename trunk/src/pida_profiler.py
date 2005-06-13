f22335f4d9ae46b66879627fe8e897f7e153676d
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2005-06-13 20:57:49 +0000

    profiler support

--- trunk/src/pida_profiler.py
+++ trunk/src/pida_profiler.py
@@ -20,15 +20,33 @@
 
 ''' The Pida profiler plugin '''
 
+#system imports
+import os
+import profile
+import cPickle as pickle
+
 # GTK imports
 import gtk
+import gobject
 
 # Pida imports
 import tree
 import plugin
+import gtkipc
+
+def script_directory():
+    def f(): pass
+    d, f = os.path.split(f.func_code.co_filename)
+    return d
+
+SCRIPT_DIR = script_directory()
+
+
 
 class Plugin(plugin.Plugin):
     NAME = 'Profiler'
+    DICON = 'profile', 'Profile the current buffer.'
+    ICON = 'profile'
     
     def populate_widgets(self):
 
@@ -40,3 +58,57 @@ class Plugin(plugin.Plugin):
 
         self.sortascending = gtk.CheckButton()
         sb.pack_start(self.sortascending)
+
+        self.profiler = Profiler(self.cb)
+        self.fn = None
+        self.readbuf = ''
+
+    def cb_alternative(self, *args):
+        #if self.fn and self.fn.endswith('.py'):
+        self.profiler.run(self.fn, self.cb_read, self.cb_hup)
+            
+    def cb_read(self, fd, cond):
+        s = os.read(fd, 1024)
+        self.readbuf = ''.join([self.readbuf, s])
+        return True
+
+    def cb_hup(self, fd, cond):
+        print hup
+        os.close(fd)
+        t = self.readbuf
+        self.readbuf = ''
+        self.reaceived_profile(t)
+        return False
+    
+    
+
+    def evt_bufferchange(self, nr, name):
+        self.fn = name
+
+
+class Profiler(object):
+
+    def __init__(self, cb):
+        self.cb = cb
+        self.ipc = gtkipc.IPWindow(self)
+
+    def run(self, filename, ioreadcb, iohupcb):
+        profilerfn = os.path.join(SCRIPT_DIR, 'profiler.py')
+        xid = '%s' % self.ipc.get_lid()
+        py = self.cb.opts.get('commands', 'python')
+        pid = os.fork()
+        if pid == 0:
+            os.execvp(py, ['python', profilerfn, filename, xid])
+        else:
+            self.pid = pid
+        print 'run'
+
+    def do_stats(self, outf):
+        f = open(outf, 'r')
+        stats = pickle.load(f)
+        f.close()
+        os.remove(outf)
+        print 'stats', stats
+
+
+
---
+++ trunk/src/profiler.py
@@ -0,0 +1,56 @@
+#Copyright (c) 2005 Ali Afshar aafshar@gmail.com
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
+import profile
+import os
+import sys
+import gtkipc
+import tempfile
+import pickle
+
+
+class Profiler(object):
+    def __init__(self, sid):
+        self.ipc = gtkipc.IPWindow(self)
+        self.ipc.reset(long(sid))
+        self.ipc.connect()
+
+    def profile(self, filename):
+        p = profile.Profile()
+        com = 'execfile("%s")' % filename
+        p.run(com)
+        p.snapshot_stats()
+        outf = tempfile.mktemp('pida-profile')
+        f = open(outf, 'w')
+        s = pickle.dump(p.stats, f)
+        f.close()
+        self.ipc.write('stats', outf, 8)
+        while gtk.events_pending():
+            gtk.main_iteration()
+
+
+def main(filename, sid):
+    profiler = Profiler(sid)
+    profiler.profile(filename)
+
+if __name__ == '__main__':
+    print sys.argv
+    main(sys.argv[1], sys.argv[2])
