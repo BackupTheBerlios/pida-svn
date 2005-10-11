e249a57fb1ea6e4d06f46da3bb5e1726c00626b9
cogumbreiro <cogumbreiro@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2005-10-11 12:00:07 +0000

    Added more compatibility with peak.binding

--- trunk/src/plugins/culebra/components.py
+++ trunk/src/plugins/culebra/components.py
@@ -19,7 +19,62 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+"""
+This module tries to offer a very small subset of existing facilities of
+'peak.binding'.
 
+Its propose is to act as a bridge to aid in the migration to PEAK. Or to help
+users who only need what this module has to offer.
+
+You should import this module as:
+try:
+    from peak.api import binding
+except ImportError:
+    import components as binding
+
+Currently supported stuff:
+
+binding.Component (someParent, foo="foo", bar=1)
+binding.Obtain ("../some/path")
+binding.Obtain (SomeClass)
+binding.Obtain (PropertyName ("some.key"))
+binding.Obtain (Foo, offerAs=[ISomeIterface])
+binding.Obtain (Foo, offerAs=[PropertyName('some.key')])
+binding.Make (SomeClass)
+binding.Make (lambda self: "foo")
+binding.Make (Foo, offerAs=[ISomeIterface])
+binding.Make (Foo, offerAs=[PropertyName('some.key')])
+
+Not suported:
+binding.Make (dict)
+binding.Make (list)
+binding.Make (lambda: "foo")
+binding.Make (Foo, uponAssembly = True)
+
+What's new and shouldn't be used if you're concerned with compatibility:
+binding.Obtain ("foo", static = False)
+binding.Obtain ("foo()")
+binding.PropertyWrapper()
+binding.ObjectPath()
+binding.Component.iterUp()
+binding.Component.components
+binding.Component.namedComponents
+
+Q: Why not use the actual peak.binding module?
+A: On some specific cases size is a problem, on other cases the problem is that
+PEAK is, unfornutely, not available on distributions.
+
+Q: But isn't peak.binding more throughly tested? Aren't we just introducing more
+bugs into it?
+
+A: This is a real small module, I am going to port the test cases that apply to
+peak.binding so that it has the same level of unittesting the original module
+has.
+
+Q: Can you implement X?
+A: Maybe, but only if it's really small and offers real gain, otherwise please
+use peak.binding instead.
+"""
 import weakref
 
 void_method = lambda self: None
@@ -53,19 +108,33 @@ class ObjectPath (object):
     If you assign the 'static' keyword argument to True then the
     value is only retrieved once.
     """
-    def __init__ (self, path, static = False):
+    
+    def __init__ (self, path, static = True, offerAs = (), uponAssembly = False):
         self.fullPath = path.split("/")
         self.isAbsolute = path.startswith("/")
-        self.isStatic = static
+        self.keys = offerAs
+        # The return values and
+        self.returnValues = {}
+        self.areStatic = {}
+        self.static = static
+        self.uponAssembly = uponAssembly
     
     def transverse (self, full_path, helper_func):
         
         return obj
+    
+    def isStatic (self, obj):
+        return self.areStatic.get (obj, self.static)
+    
+    def hasReturnValue (self, obj):
+        return obj in self.returnValues
         
     def __get__ (self, obj, type = None):
+        
+        if self.isStatic (obj) and self.hasReturnValue (obj):
+            return self.returnValues[obj]
 
-        if self.isStatic and hasattr(self, "returnValue"):
-            return self.returnValue
+        orig_obj = obj
             
         if self.isAbsolute:
             obj = getRootComponent(obj)
@@ -84,10 +153,15 @@ class ObjectPath (object):
                 if is_callable:
                     obj = obj()
         
-        if self.isStatic:
-            self.returnValue = obj
+        if self.static:
+            
+            if hasattr (self, "name"):
+                setattr (orig_obj, self.name, obj)
+            else:
+                self.returnValues[orig_obj] = obj
             
         return obj
+
 
 class PropertyWrapper (object):
     """
@@ -123,7 +197,118 @@ class PropertyWrapper (object):
         obj = self.getVariable (obj)
         getattr (obj, self.setter) (value)
 
+class Make (object):
+    """
+    A 'Make' content is created only when it's needed. It should wrap the
+    component that you want to make lazy.
+    
+    Usage example:
+    
+    class Bar (Component):
+        pass
+    
+    class Foo (Component):
+        bar = Make (Bar)
+    
+    When you create an instance of 'Foo', the 'bar' instance will only be
+    created when you access it the first time.    
+    """
+    
+    def __init__ (self, componentFactory, offerAs = (), uponAssembly = False):
+        self.componentFactory = componentFactory
+        self.keys = offerAs
+        self.uponAssembly = uponAssembly
+        self.components = {}
+        
+    def __get__ (self, obj, type = None):
+        if obj in self.components:
+            return self.components[obj]
+        
+        self.components[obj] = self.componentFactory (obj)
+        
+        if hasattr (self, "name"):
+            setattr (obj, self.name, self.components[obj])
 
+        return self.components[obj]
+
+class MetaComponent (type):
+    def __init__ (cls, name, bases, attrs):
+        cls._properties = {}
+        cls._assembly_components = []
+        
+        for key, value in attrs.iteritems ():
+            if isinstance (value, Make) or isinstance (value, ObjectPath):
+                value.name = key
+                
+                for prop in value.keys:
+                    cls._properties[prop] = value
+                
+                if (isinstance (value, Make) or isinstance (value, ObjectPath)) and value.uponAssembly:
+                    cls._assembly_components.append (value)
+                    
+        # chain up
+        super (MetaComponent, cls).__init__ (cls, name, bases, attrs)
+        
+class ParentsIterator:
+    def __init__ (self, component):
+        self.component = component
+        
+    def next (self):
+        component = self.component
+        if component is None:
+            raise StopIteration
+        
+        self.component = self.component.parent
+        
+        return component
+    
+    def __iter__ (self):
+        return self
+        
+class PropertyMapper (object):
+    def __init__ (self, parent, data):
+        self._data = data
+        self._parent = weakref.ref (parent)
+    
+    parent = property (lambda self: self._parent())
+    
+    def get (self, key, default = None):
+        if key not in self:
+            return default
+        
+        return self[key]
+        
+    def __getitem__ (self, key):
+        data = self._data[key]
+        if hasattr (data, "__get__"):
+            return data.__get__ (self.parent)
+        else:
+            return data
+     
+    def __setitem__ (self, key, value):
+        self._data[key] = value
+        
+    def __contains__ (self, key):
+        return key in self._data
+    
+    def __len__ (self):
+        return len (self._data)
+    
+    def has_key (self, key):
+        return self._data.has_key (key)
+    
+    def __repr__ (self):
+        assert self._data is not None
+        print "<PropertyMapper %r>" % self._data
+
+try:
+    import gobject
+    
+    class MetaGComponent (MetaComponent, gobject.GObjectMeta): pass
+    
+except ImportError:
+    gobject = None
+    
 class Component (object):
     """
     A Component is an object that is structured in a hierarchical model.
@@ -134,14 +319,32 @@ class Component (object):
     It also contains a '_components' protected variable that holds a list of its
     children components.
     """
+    if gobject is not None:
+        __metaclass__ = MetaGComponent
+    else:
+        __metaclass__ = MetaComponent
     
-    def __init__ (self, parent = None):
+    _properties = {}
+    
+    def __init__ (self, parent = None, **kwargs):
         self.__parent = parent is not None and weakref.ref (parent) or void_func
+        self._properties = PropertyMapper (self, self._properties)
         # Maintain components reference
         self._components = []
         for component in self.components:
-            self._components.append (component(self))
-            
+            self._components.append (component (self))
+        
+        asm_components = []
+        for component in self._assembly_components:
+           asm_components.append (component.__get__ (self))
+        
+        self._assembly_components = asm_components
+        
+        for attr, component in self.namedComponents.iteritems ():
+            setattr (self, attr, component(self))
+        
+        for key, value in kwargs:
+            setattr (self, key, value)
         self._init ()
     
     def _init (self):
@@ -149,8 +352,46 @@ class Component (object):
     
     def getParent (self):
         return self.__parent ()
-        
+    
     parent = property (getParent)
     
     components = ()
+    namedComponents = {}
+
+    def iterUp (self):
+        return ParentsIterator (self)
+        
+
+# Peak compatibility:
+LazyComponent = Make
+
+class PropertyName:
+    def __init__ (self, name):
+        self.name = name
+    
+    def __repr__ (self):
+        return "<Property('%s')>" % self.name
+
+class ObtainProperty (object):
+    def __init__ (self, key):
+        self.key = key
+        
+    def __get__ (self, obj, type = None):
+        for component in iter (obj.iterUp ()):
+            val = component._properties.get (self.key)
+            if val is not None:
+                break
+        
+        # Cache it when needed
+        if component is not obj and val is not None:
+            obj._properties[self.key] = val
+            
+        return obj._properties[self.key]
+            
+
+def Obtain (key, **kwargs):
+    if isinstance (key, str):
+        return ObjectPath (key, **kwargs)
+    elif isinstance (key, PropertyName):
+        return ObtainProperty (key, **kwargs)
 
