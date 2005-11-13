a277a60ef90ab7ed6e27262daa946acc851b3747
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2005-11-13 16:13:26 +0000

    bumped version

--- trunk/setup.py
+++ trunk/setup.py
@@ -73,7 +73,7 @@ for plugin in os.listdir(plugindir):
 
 log('Performing setup.')
 setup(name='pida',
-    version='0.2.2',
+    version='0.2.3',
     author='Ali Afshar',
     author_email='aafshar@gmail.com',
     url='http://pida.berlios.de',
