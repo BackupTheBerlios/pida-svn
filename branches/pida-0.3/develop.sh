c0fc75cbcdaa7e029d11367ce8a389df3dcb995a
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2005-12-12 16:44:42 +0000

    pass on command line arguments to pida in the develop.sh script

--- branches/pida-0.3/develop.sh
+++ branches/pida-0.3/develop.sh
@@ -5,4 +5,4 @@ python setup.py build 2>&1>/dev/null
 python setup.py bdist --dist-dir=$distdir --formats=egg 2>&1>/dev/null
 export PYTHONPATH=$distdir/`ls $distdir`
 echo built and added $PYTHONPATH to '$PYTHONPATH'
-python scripts/pida
+python scripts/pida $*
