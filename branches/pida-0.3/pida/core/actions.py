f3135b4b7081a8efedc5be92a97b2307b8a1c49b
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-01-07 13:19:39 +0000

    tidied actiongroup module

--- branches/pida-0.3/pida/core/actions.py
+++ branches/pida-0.3/pida/core/actions.py
@@ -21,8 +21,10 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
+# gtk import(s)
 import gtk
 
+# pida core import(s)
 import base
 
 class action_handler(base.pidacomponent):
