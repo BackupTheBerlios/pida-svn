2ac3d68c747ab9210babb3d50adb39df3cea570f
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-01-18 12:30:19 +0000

    added openwith service that does nothing for now

---
+++ trunk/pida/services/openwith.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*- 
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+#Copyright (c) 2006 The PIDA Project 
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import pida.core.service as service
+import gtk
+import os
+defs = service.definitions
+
+class open_with(service.service):
+
+    def init(self):
+        self.__datafile = os.path.join(self.boss.pida_home,
+                                       'data', 'openwith')
+
+    def cmd_get_openers(self, filename):
+        pass
+
+    def cmd_get_openers_menu(self, filename):
+        pass
+
+Service = open_with
