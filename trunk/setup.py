71b8267266ca702559299f0830e67ba0dce7a19a
mnemoc <mnemoc@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-02-01 21:16:10 +0000

    * removed 'rc1' from version string

--- trunk/setup.py
+++ trunk/setup.py
@@ -25,7 +25,7 @@
 import os
 import sys
 
-VERSION_STRING = '0.3.0-rc1'
+VERSION_STRING = '0.3.0'
 
 def ensure_version_file_exists():
     """Check for existence of version file and create if unavailable."""
