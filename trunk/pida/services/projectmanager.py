86e0fa158dabe38100966eccfc7e6d56e9603d26
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-02-03 02:45:25 +0000

    moved the project toolbar dropdown

--- trunk/pida/services/projectmanager.py
+++ trunk/pida/services/projectmanager.py
@@ -175,8 +175,11 @@ class ProjectManager(service.service):
     def __init_project_toolbar(self):
         tb = self.boss.call_command('window', 'get_ui_widget',
                                path='/toolbar')
+        ph = self.boss.call_command('window', 'get_ui_widget',
+                               path='/toolbar/VcToolbar')
+        i = tb.get_item_index(ph)
         ti = gtk.ToolItem()
-        tb.insert(ti, -1)
+        tb.insert(ti, i)
         ti.set_expand(True)
         self.__projects_combo = gtk.ComboBox()
         ti.add(self.__projects_combo)
