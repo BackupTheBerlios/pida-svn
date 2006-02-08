8a44c6c608cabc5dbd4c46895c138d99c68fdf89
aafshar <aafshar@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-02-08 12:29:14 +0000

    added a few more tests to tree

--- trunk/tests/tree_test.py
+++ trunk/tests/tree_test.py
@@ -3,6 +3,14 @@
 
 import pida.utils.testing as testing
 import pida.pidagtk.tree as tree
+import gtk
+import gobject
+class MockItem(object):
+
+    def __init__(self, k, n):
+        self.key = k
+        self.n = n
+        
 
 class TreeTest1(testing.GtkTestCase):
 
@@ -16,4 +24,69 @@ class TreeTest1(testing.GtkTestCase):
         self.t.set_property('markup-format-string', 'banana')
         mfs = self.t.get_property('markup-format-string')
         self.assertEqual(mfs, 'banana')
+
+    def test_3_add_item(self):
+        self.t = tree.Tree()
+        self.assertEqual(len(self.t.model), 0)
+        i = MockItem('a', 'b')
+        self.t.add_item(i)
+        self.assertEqual(len(self.t.model), 1)
+        for row in self.t.model:
+            self.assertEqual(row[0], 'a')
+            self.assertEqual(row[1].value, i)
+
+    def test_4_add_item_with_key(self):
+        self.t = tree.Tree()
+        i = MockItem('a', 'b')
+        self.t.add_item(i, key='z')
+        self.assertEqual(len(self.t.model), 1)
+        for row in self.t.model:
+            self.assertEqual(row[0], 'z')
+            self.assertEqual(row[1].value, i)
+
+    def test_5_selected_key(self):
+        self.t = tree.Tree()
+        i = MockItem('k1', 'b')
+        self.t.add_item(i)
+        i = MockItem('k2', 'c')
+        self.t.add_item(i)
+        self.assertEqual(self.t.selected_key, None)
+        self.t.set_selected('k1')
+        self.assertEqual(self.t.selected_key, 'k1')
+        self.t.set_selected('k2')
+        self.assertEqual(self.t.selected_key, 'k2')
+
+    def test_6_selected(self):
+        self.t = tree.Tree()
+        i = MockItem('k1', 'b')
+        self.t.add_item(i)
+        i2 = MockItem('k2', 'c')
+        self.t.add_item(i2)
+        self.assertEqual(self.t.selected, None)
+        self.t.set_selected('k1')
+        self.assertEqual(self.t.selected.value, i)
+        self.assertNotEqual(self.t.selected.value, i2)
+        self.t.set_selected('k2')
+        self.assertEqual(self.t.selected.value, i2)
+        self.assertNotEqual(self.t.selected.value, i)
+
+    def test_7_clicked(self):
+        self.t = tree.Tree()
+        i = MockItem('k1', 'b')
+        self.t.add_item(i)
+        i2 = MockItem('k2', 'c')
+        self.t.add_item(i2)
+        self.assertEqual(self.t.selected, None)
+        def _c(tv, item):
+            # check it is None now
+            self.assertEqual(tv.selected, None)
+            def _i():
+                # but is getting set in idle
+                self.assertEqual(tv.selected, item)
+            gtk.idle_add(_i)
+        self.t.connect('clicked', _c)
+        self.t.emit('clicked', self.t.model[0][1])
+
+
+
         
