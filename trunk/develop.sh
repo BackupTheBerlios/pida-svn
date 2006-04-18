5f687d84a3d6ce1185cef763d713422f719e7969
mnemoc <mnemoc@ef0b12da-61f9-0310-ba38-b2629ec279a7>
2006-04-18 14:37:41 +0000

    * added some output messages on develop.sh

--- trunk/develop.sh
+++ trunk/develop.sh
@@ -3,6 +3,7 @@
 # don't accept errors
 set -e
 
+echo "Preparing pida..."
 # detect the real location of the working copy
 if type -p readlink > /dev/null; then
 	ME=$( readlink -f "$0" )
@@ -22,9 +23,12 @@ tag_svn_revision = true
 EOT
 
 (
+echo "Building pida..."
 cd "$PIDADIR"
 python setup.py develop --install-dir=run --script-dir=run 2>&1>run/buildlog.log
 rm setup.cfg
 grep '^Version:' pida.egg-info/PKG-INFO | cut -d' ' -f2- > pida/data/version
 )
+
+echo "Running..."
 python "$PIDADIR/run/pida" $*
